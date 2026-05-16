d6944bd51f2044f2fb0b6ceeb8a6dd59ad17a63d
Simon08 <simonpierresagno08@gmail.com>
2026-05-16 17:00:47 +0000

    doc

--- config/db.config.js
+++ config/db.config.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const connnectDB = async ()=>{
     try {
-        await mongoose.connect(`${process.env.DB_URL}/${process.env.DB_NAME}`)
+        await mongoose.connect(`${process.env.DB_URL}`)
         console.log("Database connected successfully")
     } catch (error) {
         console.log(error);
